0c04ffdd184d20495317c253fab81b4c03c3a4b1
Ayodeji <ejiadedeji@gmail.com>
2020-11-19 17:01:43 -0600

    update

--- app.R
+++ app.R
@@ -10,7 +10,7 @@ log_init()
 ui <- dashboardPage(dashboardHeader(title = "Exam Center"),
                     dashboardSidebar(
                       sidebarMenu(
-                        menuItem("Home", tabName = "home", icon = icon("table")),
+                        menuItem("Home", tabName = "Home", icon = icon("table")),
                         menuItem("Settings", tabName = "readMe", icon = icon("cog"))
                       )
                     ),
@@ -19,7 +19,15 @@ ui <- dashboardPage(dashboardHeader(title = "Exam Center"),
                       useShinyjs(),
                       tabItems(
                         tabItem(tabName = "Home",
+                                fluidRow(
+                                  box(id = "myVideo",
+                                      tags$video(width = "640", height = "480", autoplay = T),
+                                      tags$canvas(id = "canvas", width = "640", height = "480"),
+                                      tags$button(id = "snap", width = "50"),
+                                      includeScript(path = "video.js"),
 
+                                  ),
+                                )
 
 
                         ),
@@ -34,7 +42,6 @@ ui <- dashboardPage(dashboardHeader(title = "Exam Center"),
 server <- function(input, output) {
   set_logging_session()
   options(shiny.maxRequestSize = 30 * 1024^2)
-
 
 
 }
