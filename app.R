66d877de70f4be874bcc3fe8be98c95d0d152c08
Ayodeji <ejiadedeji@gmail.com>
2020-11-29 00:10:29 -0600

    update

--- app.R
+++ app.R
@@ -3,48 +3,97 @@ library(shinydashboard)
 library(shinyMatrix)
 library(shinyjs)
 library(shinyEventLogger)
-
 set_logging()
 log_init()
 
-  ui <- dashboardPage(dashboardHeader(title = "Exam Center"),
-                    dashboardSidebar(
-                      sidebarMenu(
-                        menuItem("Home", tabName = "Home", icon = icon("table")),
-                        menuItem("Settings", tabName = "readMe", icon = icon("cog"))
-                      )
-                    ),
-
-                    dashboardBody(
-                      useShinyjs(),
-                      tabItems(
-                        tabItem(tabName = "Home",
-                                fluidRow(
-                                  column(8, offset = 2,
-                                         box(id = "myVideo", width = 12, height = NULL , collapsible = T, solidHeader = T,
-                                             div(align = 'center',
-                                                 tags$video(id = "video", autoplay = T, width = "100%", height="100%"),
-                                                 tags$canvas(id = "canvas"),
-                                                 includeScript(path = "video.js"),
-                                                 tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "style.css"))
-                                             )
-                                         ),
-                                  ),
+ui <- dashboardPage(
+  dashboardHeader(title = "Exam Center"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Home", tabName = "Home", icon = icon("table")),
+    menuItem("Settings", tabName = "readMe", icon = icon("cog"))
+  )),
+  
+  dashboardBody(useShinyjs(),
+                tabItems(
+                  tabItem(tabName = "Home",
+                          fluidRow(column(
+                            8,
+                            offset = 2,
+                            box(
+                              id = "myVideo",
+                              width = 12,
+                              height = NULL ,
+                              collapsible = T,
+                              solidHeader = T,
+                              div(
+                                align = 'center',
+                                tags$video(
+                                  id = "video",
+                                  autoplay = T,
+                                  width = 640,
+                                  height = 480
                                 ),
-
-                        ),
-                        tabItem(tabName = "Settings",
-
-                        )
-                      ),
-                    )
+                                
+                                tags$canvas(id = "canvas", width = 640, height = 480),
+                                
+                                includeScript(path = "video.js"),
+                                
+                                tags$head(
+                                  tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+                                ),
+                                
+                                textAreaInput('imgStr', 'Label'),
+                                
+                                actionButton('startExam', 'Start')
+                                
+                                
+                              )
+                            ),
+                          ),),),
+                  tabItem(tabName = "Settings",)
+                ),)
 )
 
 server <- function(input, output) {
   set_logging_session()
-  options(shiny.maxRequestSize = 30 * 1024^2)
+  options(shiny.maxRequestSize = 30 * 1024 ^ 2)
+  
+  
+  observeEvent(input$startExam, {
+    interval = 5
+    
+    
+    
+    repeat {
+      startTime = Sys.time()
+      
+      runjs('
+      
+             var video = document.getElementById("video");
+             console.log("hi")
+             var canvas = document.getElementById("canvas");
+             var context = canvas.getContext("2d");
+             context.drawImage(video, 0, 0);
+             str2 = (canvas.toDataURL());
+             str3 = str2.split(",").pop();
+             console.log(str3);
+             Shiny.setInputValue("imgStr", str3);
+             console.log("checking");
 
 
+    ')
+      
+      log_message(input$imgStr)
+      
+      sleepTime = startTime + interval - Sys.time()
+      if (sleepTime > 0)
+        Sys.sleep(sleepTime)
+      
+    }
+    
+  })
+  
+  
 }
 
 shinyApp(ui, server)
--- video.js
+++ video.js
@@ -1,31 +1,33 @@
 var video = document.getElementById('video');
 
-    // Get access to the camera
-    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
-            video.srcObject = stream;
-            video.play();
-        });
-    }
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    var video = document.getElementById('video');
-    var flag = false;
-
-    document.getElementById("start").addEventListener("click", function() {
-        draw();
+// Get access to the camera
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({
+        video: true
+    }).then(function(stream) {
+        video.srcObject = stream;
+        video.play();
     });
+}
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+var flag = false;
 
-    document.getElementById("stop").addEventListener("click", function() {
-        flag = true;
-    });
+document.getElementById("start").addEventListener("click", function() {
+    draw();
+});
 
-    function draw() {
-        context.drawImage(video, 0, 0, 640, 480)
-        if(!flag){
-            setTimeout(draw, 2000)
-        }
-        else{
-            context.clearRect(0, 0, 640, 480);
-        }
+document.getElementById("stop").addEventListener("click", function() {
+    flag = true;
+});
+
+function draw() {
+    context.drawImage(video, 0, 0, 640, 480)
+    if (!flag) {
+        setTimeout(draw, 2000)
+    } else {
+        context.clearRect(0, 0, 640, 480);
     }
+    
+}
